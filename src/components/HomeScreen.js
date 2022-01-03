b6db40cea961f1a1fab3add779e3c8feeaeff6a4
pedroTest1 <agustinnicolas753@hotmail.com>
2022-01-03 19:03:34 -0300

    test

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const HomeScreen = () => {
     return (
         <div>
-            <h2> Home Screen </h2> 
+            <h2> Home Screen a</h2> 
         </div>
     )
 }
